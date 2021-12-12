63a5f8d9a86477c425e29df93ecd430be759d765
K. Faryab Haye 🏓 <kfaryabh@umich.edu>
2021-12-12 17:27:47 -0500

    quick fix

--- model.py
+++ model.py
@@ -33,16 +33,26 @@ class XLNetForMultiLabelClassification(XLNetPreTrainedModel):
             head_mask=head_mask,
             inputs_embeds=inputs_embeds,
         )
+
+        print(outputs)
+
         pooled_output = outputs[1]
+
+        print(pooled_output)
 
         # pooled_output = self.dropout(pooled_output)
         logits = self.classifier(pooled_output)
 
+        print(logits)
+
         outputs = (logits,) + outputs[2:]  # add hidden states and attention if they are here
+
+        print(outputs)
 
         if labels is not None:
             loss = self.loss_fct(logits, labels)
             outputs = (loss,) + outputs
+        
 
         return outputs  # (loss), logits, (hidden_states), (attentions)
 
